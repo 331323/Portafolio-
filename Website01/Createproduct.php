072d6517e7b9f2da75ac45f3238c831d09864571
331323 <73833779+331323@users.noreply.github.com>
2022-04-01 12:24:56 +0200

    in progress

---
+++ Website01/Createproduct.php
@@ -0,0 +1,24 @@
+<?php
+include_once("./includes/meta.php");
+include_once("./includes/sidebar.php");
+include_once("./includes/header.php");
+
+if(isset($_POST["submit"])){
+  // define variables for the query
+  $name = cleaning($_POST['name']);
+  $stock = cleaning($_POST["stock"]);
+  // execute query with cleaned variables
+  mysqli_query($conn, "INSERT INTO `stock` (`id`, `itemname`, `totalstock`, `totalavailability`)
+  VALUES (NULL, '$name', '$stock', '1')");
+}
+
+?>
+
+<form action="" method="POST">
+    <input class="form-control" type="text" name="name" placeholder="Itemname">
+    <br>
+    <input class="form-control" type="text" name="stock" placeholder="Totalstock">
+    <button class="btn btn-primary" type="submit" name="submit">Confirm</button>
+</form>
+
+ <?php include_once("./includes/footer.php"); ?>
---
+++ Website01/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once("./includes/meta.php");
+include_once("./includes/sidebar.php");
+include_once("./includes/header.php");
+
+// hier je php code
+
+ ?>
+
+ <!-- hier je eigen code -->
+ Dashboard pagina
+
+ <?php include_once("./includes/footer.php"); ?>
